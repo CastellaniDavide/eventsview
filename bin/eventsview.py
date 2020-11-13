6c373c070014d46cc022bc1ae077a2057263394c
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-11-13 08:57:49 +0100

    Created bin/template.py

---
+++ bin/eventsview.py
@@ -0,0 +1,19 @@
+"""eventsview
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2020-11-13"
+
+class eventsview:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(eventsview.eventsview())
+	
+	def eventsview():
+		"""eventsview first funtion
+		"""
+		return "eventsview" #for the test (see test_eventsview.py)
+		
+if __name__ == "__main__":
+	eventsview()
