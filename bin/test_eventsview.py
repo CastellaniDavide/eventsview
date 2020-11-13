e1251210826c54a459578683682aa66dc2099df0
DavideC03 <davide.castellani.5.8.03@gmail.com>
2020-11-13 22:06:26 +0100

    v01.01

--- bin/test_eventsview.py
+++ bin/test_eventsview.py
@@ -10,7 +10,17 @@ def test():
 	Write here all test you want to do.
 	REMEMBER to test your programm you can't use __init__ function
 	"""
-	assert eventsview.eventsview() == "eventsview", "test failed"
+	assert eventsview.check_PC("localhost", 1) == True, "Checker PC fails (personal_code: 1)"
+	assert eventsview.check_PC("localhost", 2) == True, "Checker PC fails (personal_code: 1)"
+	assert eventsview.check_PC("localhost", 10) == True, "Checker PC fails (personal_code: 1)"
+
+	assert eventsview.make_csv_standard("'test','test'\n'example value','example value 2'\n'example value','example value 2'\n'example value','example value 2'") == "'test','test'\n'example value','example value 2'\n'example value','example value 2'\n'example value','example value 2'", "Maker CSV standard fails (personal_code: 2)"
+
+	assert eventsview.csv2array("'test','test'\n'example value','example value 2'\n'example value','example value 2'\n'example value','example value 2'") == [["'test'","'test'"], ["'example value'","'example value 2'"], ["'example value'","'example value 2'"], ["'example value'","'example value 2'"]], "CSV2Array fails (personal_code: 3a)"
+	
+	assert eventsview.array2csv([["test","test"], ["example value","example value 2"], ["example value","example value 2"], ["example value","example value 2"]]) == '"test","test"\n"example value","example value 2"\n"example value","example value 2"\n"example value","example value 2"\n', "Array2CSV fails (personal_code: 3b)"
+
+	assert eventsview(), "test failed (personal_code: 0)"
 	#assert eventsview.<function>(<values>) == <the result(s) you would like to have>, "<the fail message>"
 	
 if __name__ == "__main__":
